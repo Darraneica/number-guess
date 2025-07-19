7e031dff6342e35f0dbe99c9b288f8ab58fa1640
Darraneica <119922086+Darraneica@users.noreply.github.com>
2025-07-19 18:38:10 -0500

    Update number-guess.py

--- number-guess.py
+++ number-guess.py
@@ -1,30 +1,27 @@
-import random
+import random 
 
-class Guess:
- 
-    def greeting(self):
-        self.name = input("What is your name? ")
-    
-    def guess_game(self):
-        num = random.randint(1,50)
-        chances = 10
-        guesses = 0
+class Game:
+       
+    def guessNum(self):
+        self.random = random.randint(1, 10)
+        count = 0
+        attempts = 4
 
-        while guesses < chances: 
-            guesses += 1
-            self.prompt = int(input(f"Can you guess the number, {self.name}? "))
-            
-            if self.prompt == num:
-                print(f"You guessed it correctly! The correct number is {num}. You guessed it in {guesses} attempts!")
-                break
-            elif guesses >= chances and self.prompt != num:
-                print(f"Try again! The number was {num}.")
-            elif self.prompt > num:
-                print("You've guessed too high. Try a lower number.")
-            elif self.prompt < num:
-                print("You've guessed too low. Try a higher number.")
+        while attempts > count:
+            count += 1
 
-guess = Guess()
-guess.greeting()
-guess.guess_game()
-        
+            number = int(input("Guess the number: ")) # the input method where users add their guess.
+
+            if number == self.random: # if the number matches the selected guess
+                print(f"Correct! The number is {self.random}. It took you {attempts} attempts.") # print the number + attempts
+                break # break is used to stop the loop since the gueess was answered correctly.
+            elif number > attempts and number != self.random: # if number exceeds the 4 attempts and the number is not the right guess
+                print(f"Game over. The number was {self.random}.") # print the game is over + print the correct number
+            elif number > self.random: # if your number is higher than the selected number 
+                print("Your guess it too high. Try again.") # print too high
+            elif number < self.random: # if your number is lower than the selected number
+                print("Your guess is too low. Try again.") # print too low
+                        
+
+guess_num = Game() # calls the Game class with an object
+print(guess_num.guessNum()) # prints the object + method
